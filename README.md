3ec0f80e92b262b683179fd3a42689ff1f66a029
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2024-01-21 14:59:06 +0700

    Working server framework

---
+++ README.md
@@ -0,0 +1,2 @@
+# PiTube
+This project was made as an attempt to watch Youtube on an unsupported phone. Run this on a local machine, then connect to it through browser to watch videos.
--- index.js
+++ index.js
@@ -1,11 +1,77 @@
 const fs = require('fs'),
-    http = require('http');
+    http = require('http'),
+    https = require('https'),
+    URL = require('url').URL,
+    ytdl = require('ytdl-core');
+
+const database = {
+    data: {},
+    save: () =>
+        fs.writeFileSync(
+            'database.json',
+            JSON.stringify(database.data, null, '    '),
+            'utf-8'
+        )
+}
+try {
+    database.data = JSON.parse(fs.readFileSync('database.json', 'utf-8'));
+} catch (err) {
+    console.log('Cannot read database, asssume empty')
+}
 
 http.createServer((req, res) => {
     let data = '';
     req.on('data', c => data += c);
 
     req.on('end', () => {
+        let url = new URL(req.url, `http://${req.headers.host}`);
 
+        // static client files
+        if (
+            url.pathname == '/'
+            || url.pathname.startsWith('/client')
+        ) {
+            res.write(fs.readFileSync('.' + url.pathname, 'utf-8'));
+            return res.end();
+        }
+
+        let id = url.pathname.slice(1); // slice the slash
+
+        if (!ytdl.validateID(id)) {
+            res.writeHead(404, 'URL not found');
+            res.write('URL not found');
+            return res.end();
+        }
+
+        if (database.data[id]) {
+            // if id exists in database, parse expire
+            let vid = new URL(database.data[id])
+            console.log(vid.searchParams.get('expire'));
+            if (vid.searchParams.get('expire') > new Date() / 1000) {
+                res.writeHead(302, {
+                    'Location': database.data[id]
+                })
+                return res.end()
+            }
+        }
+
+        return
+
+        ytdl.getInfo(`https://youtube.com/watch?v=${id}`).then(info => {
+            console.log(info.videoDetails.title)
+            let urls = []
+            info.formats.forEach(vid => {
+                if (
+                    vid.hasAudio && vid.hasVideo
+                    && vid.container == 'mp4'
+                )
+                    urls.push(vid.url)
+            });
+            urls.forEach(value => res.write(value + '\n\n'));
+            res.end();
+
+            database.data[id] = urls[0];
+            database.save();
+        })
     })
-}).listen(8080)
+}).listen(8080);
